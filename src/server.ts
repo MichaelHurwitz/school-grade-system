15fb11cb04048e985881f3ba2465cab8a03e2aca
Michael Hurwitz <amh77054@gmail.com>
2024-10-13 10:26:45 +0300

    adding server.ts and installing all necessary packages

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser'; 
+import userRoutes from './routes/userRoutes';
+import  swaggerUi  from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());  
+
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Class Grades API',
+      version: '1.0.0',
+      description: 'API for managing user roles, grades, and more',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', 
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], 
+};
+
+const swaggerSpec = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+
+
+mongoose
+  .connect(process.env.DB_URI as string)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use('/api/users', userRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
